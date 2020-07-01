ff437c01d4ef91801e5a62225a99ed0fac14ec4a
Daniel Kraemer <dkraemer@dkross.org>
2020-07-01 19:11:39 +0200

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,18 +6,18 @@ set -o xtrace
 cp -fv .devcontainer/.bashrc /root/
 
 # TypeScript development
-#yarn global add typescript
+yarn global add typescript
 #yarn global add @types/node
 #yarn global add tsdx
-#yarn global add ts-node
+yarn global add ts-node
 #yarn global add oclif
 
 # Misc
 #yarn global add gulp
 
 # Angular
-#yarn global add @angular/cli
-#yarn global add http-server
+yarn global add @angular/cli
+yarn global add http-server
 
 # Google Firebase
-#yarn global add firebase-tools
+yarn global add firebase-tools
